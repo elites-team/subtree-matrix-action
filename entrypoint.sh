340dd9635f93686f7edf66d9035eaee0f5778450
noopy0415 <noopy0415@gmail.com>
2021-01-26 13:25:02 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,12 +29,15 @@ else
 fi
 
 # Sync subtree directory
-echo "----------------git subtree $INPUT_ACTION----------------------"
 before_sha=`git rev-parse --short HEAD`
+echo "----------------git subtree pull----------------------"
 git subtree pull --prefix="$INPUT_PATH" git@github.com:"$INPUT_REPO".git "$PULL_BRANCH" --squash "${PULL_MESSAGE}"
+echo "----------------git subtree push----------------------"
 git subtree push --prefix="$INPUT_PATH" git@github.com:"$INPUT_REPO".git "$PULL_BRANCH"
 after_sha=`git rev-parse --short HEAD`
 if [ ! $before_sha = $after_sha ]; then
+  echo "----------------git pull----------------------"
   git pull origin $GITHUB_REF
+  echo "----------------git push----------------------"
   git push origin $GITHUB_REF
 fi
