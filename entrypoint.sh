93bbcd9d7e3e62455349ac1f9a1833046c27c9f4
noopy0415 <noopy0415@gmail.com>
2021-01-26 10:03:15 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,16 +31,16 @@ fi
 
 # Check for merge message
 if [ "$INPUT_MESSAGE" == "" ]; then
-	PULL_MESSAGE='-m "Update dependency of subtree"'
+	PULL_MESSAGE="Update dependency of subtree"
 else
-	PULL_MESSAGE='-m "$INPUT_MESSAGE"'
+	PULL_MESSAGE="$INPUT_MESSAGE"
 fi
 
 # Sync subtree directory
 echo "----------------git subtree $INPUT_ACTION----------------------"
 # ssh -i /root/.ssh/subtree git@github.com
 git log --oneline
-echo git subtree pull --prefix="$INPUT_PATH" git@github.com:"$INPUT_REPO".git "$PULL_BRANCH" --squash "$PULL_MESSAGE"
-git subtree pull --prefix="$INPUT_PATH" git@github.com:"$INPUT_REPO".git "$PULL_BRANCH" --squash "$PULL_MESSAGE"
+echo git subtree pull --prefix="$INPUT_PATH" git@github.com:"$INPUT_REPO".git "$PULL_BRANCH" --squash -m "$PULL_MESSAGE"
+git subtree pull --prefix="$INPUT_PATH" git@github.com:"$INPUT_REPO".git "$PULL_BRANCH" --squash -m "$PULL_MESSAGE"
 # git push -u origin master
 
