e8b8dc2dad188bdca3bfc39a51cf20a8901bd6f3
noopy0415 <noopy0415@gmail.com>
2021-01-26 11:00:12 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,15 +14,6 @@ ssh-add /root/.ssh/subtree
 
 git config user.email "$GITHUB_RUN_NUMBER+github-actions[bot]@users.noreply.github.com"
 git config user.name "github-actions[bot]"
-# Generate sha256 of the downstream repo name
-# SPLIT_DIR=$(echo -n "${INPUT_REPO}" | sha256sum)
-# SPLIT_DIR="${SPLIT_DIR::-3}"
-
-# Get subtree repository into split directory
-# git init --bare "${SPLIT_DIR}"
-
-# Create the subtree split branch
-# git subtree split --prefix="${INPUT_PATH}" -b split
 
 # Resolve downstream branch.
 # If not set then use the event github ref, if the ref isn't set default to master.
@@ -43,9 +34,6 @@ fi
 
 # Sync subtree directory
 echo "----------------git subtree $INPUT_ACTION----------------------"
-# ssh -T git@github.com
-# git log --oneline
-echo git subtree pull --prefix="$INPUT_PATH" git@github.com:"$INPUT_REPO".git "$PULL_BRANCH" --squash "${PULL_MESSAGE}"
 git subtree pull --prefix="$INPUT_PATH" git@github.com:"$INPUT_REPO".git "$PULL_BRANCH" --squash "${PULL_MESSAGE}"
+git log --oneline
 # git push -u origin master
-
