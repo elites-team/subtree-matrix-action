0b75a38883d91bac0b01c4ab6204730ef34413cc
noopy0415 <noopy0415@gmail.com>
2021-01-27 17:31:46 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh -l
 
-set -e
+# set -e
 
-# # Key scan for github.com
-# ssh-keyscan github.com > /root/.ssh/known_hosts
+# Key scan for github.com
+ssh-keyscan github.com > /root/.ssh/known_hosts
 
-# # Set ssh key for subtree
-# echo "${INPUT_DEPLOY_KEY}" >> /root/.ssh/subtree
-# chmod 0600 /root/.ssh/subtree
+# Set ssh key for subtree
+echo "${INPUT_DEPLOY_KEY}" >> /root/.ssh/subtree
+chmod 0600 /root/.ssh/subtree
 
-# eval `ssh-agent`
-# ssh-add /root/.ssh/subtree
+eval `ssh-agent`
+ssh-add /root/.ssh/subtree
 
-# git config user.email "$GITHUB_RUN_NUMBER+github-actions[bot]@users.noreply.github.com"
-# git config user.name "github-actions[bot]"
+git config user.email "$GITHUB_RUN_NUMBER+github-actions[bot]@users.noreply.github.com"
+git config user.name "github-actions[bot]"
 
+# # -------------for loop start---------------
 # if [ "$INPUT_BRANCH" == "" ]; then
 #   PULL_BRANCH="master"
 # else
@@ -37,7 +38,7 @@ set -e
 #   git pull origin $GITHUB_REF
 #   git push origin $GITHUB_REF
 # fi
-
+# # ------------------for loop end--------------------
 echo "-----matrix action------"
 # echo $INPUT_MATRIX
 # json作成にはヒアドキュメントを使う
@@ -70,12 +71,37 @@ echo "-----matrix action------"
 # echo "-r '. | length'"
 # echo $INPUT_MATRIX | jq -r '. | length'
 
-for matrix in $(echo "${INPUT_MATRIX}" | jq -r '.[] | @base64'); do
-  echo ${matrix}
-  path=$(echo ${matrix} | base64 -d | jq -r '.path')
-  echo $path
-  repo=$(echo ${matrix} | base64 -d | jq -r '.repo')
-  echo $repo
-  branch=$(echo ${matrix} | base64 -d | jq -r '.branch')
-  echo $branch
+for base in $(echo "${INPUT_MATRIX}" | jq -r '.[] | @base64'); do
+  matrix=$(echo ${base} | base64 -d )
+  # echo ${matrix}
+  INPUT_PATH=$(echo ${matrix} | jq -r '.path')
+  # echo $INPUT_PATH
+  INPUT_REPO=$(echo ${matrix} | jq -r '.repo')
+  # echo $INPUT_REPO
+  INOUT_BRANCH=$(echo ${matrix} | jq -r '.branch')
+  # echo $INOUT_BRANCH
+  # -------------for loop start---------------
+  if [ "$INPUT_BRANCH" == "" ]; then
+    PULL_BRANCH="master"
+  else
+    PULL_BRANCH="$INPUT_BRANCH"
+  fi
+
+  # Check for merge message
+  if [ "$INPUT_MESSAGE" == "" ]; then
+    PULL_MESSAGE="-m 'Update dependency of subtree'"
+  else
+    PULL_MESSAGE="-m '$INPUT_MESSAGE'"
+  fi
+
+  # Sync subtree directory
+  before_sha=`git rev-parse --short HEAD`
+  git subtree pull --prefix="$INPUT_PATH" git@github.com:"$INPUT_REPO".git "$PULL_BRANCH" --squash "${PULL_MESSAGE}"
+  git subtree push --prefix="$INPUT_PATH" git@github.com:"$INPUT_REPO".git "$PULL_BRANCH"
+  after_sha=`git rev-parse --short HEAD`
+  if [ ! $before_sha = $after_sha ]; then
+    git pull origin $GITHUB_REF
+    git push origin $GITHUB_REF
+  fi
+  # ------------------for loop end--------------------
 done
